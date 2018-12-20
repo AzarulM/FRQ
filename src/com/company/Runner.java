8d7962afa8bff29ad0f7bfc7b96954602cdbb1fc
AzarulM <43213109+AzarulM@users.noreply.github.com>
2018-12-20 00:01:31 -0500

    Update Runner.java

--- src/com/company/Runner.java
+++ src/com/company/Runner.java
@@ -3,7 +3,14 @@ package com.company;
 public class Runner {
 
     public static void main(String[] args) {
-
+        
+    
+    
+    Trail trail = new Trail();
+    
+        
+    System.out.println(trail.isLevelTrailSegment());
+    System.out.println(trail.isDifficult());
 
     }
 }
